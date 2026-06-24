2732b102e4ef0474bace30592d2eb38e0953bf76
Mukul Sharma <mukul.crm22@gmail.com>
2026-06-25 00:48:52 +0530

    Fix Streamlit import path

--- dashboard/app.py
+++ dashboard/app.py
@@ -1,3 +1,16 @@
+import sys
+import os
+
+ROOT_DIR = os.path.dirname(
+    os.path.dirname(
+        os.path.abspath(__file__)
+    )
+)
+
+if ROOT_DIR not in sys.path:
+    sys.path.insert(0, ROOT_DIR)
+
+
 import streamlit as st
 import pandas as pd
 import sqlite3
